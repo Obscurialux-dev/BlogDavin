c7a3c6c73e5ae2cf43c81555948149fe8ff94421
Obscurialux-dev <muhamaddavinfarizi06@gmail.com>
2025-08-13 16:40:33 +0700

    Tambahkan proteksi password untuk halaman admin

--- server.js
+++ server.js
@@ -2,19 +2,24 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const apiRoutes = require('./api');
+const adminAuth = require('./auth'); // <-- Impor middleware auth
 
 const app = express();
 const PORT = 3000;
 
 app.use(cors());
 app.use(express.json());
+
+// Rute yang diproteksi
+// Setiap kali ada yang akses /admin.html atau /edit.html, fungsi adminAuth akan dijalankan dulu
+app.get('/admin.html', adminAuth);
+app.get('/edit.html', adminAuth);
+
+// Menyajikan semua file dari folder 'public'
 app.use(express.static('public'));
 
+// Rute API
 app.use('/api', apiRoutes);
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
 
 app.listen(PORT, () => {
   console.log(`Server berjalan di http://localhost:${PORT}`);
